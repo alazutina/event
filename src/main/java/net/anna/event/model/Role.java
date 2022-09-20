ab732c03b205ee5050fdc8a203fbda5a0792b454
Anna Lazutina <alazutina@yandex.ru>
2022-09-20 15:04:37 +0300

    0.0

---
+++ src/main/java/net/anna/event/model/Role.java
@@ -0,0 +1,16 @@
+package net.anna.event.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="roles")
+@Data
+public class Role extends BaseEntity{
+
+    @Column(name="name")
+    private  String name;
+}
